36e9930b7554b2efad2977d8a4dc81e7a782590c
toanmap123 <toanmapbanh1@gmail.com>
2023-12-26 09:09:46 +0700

    upload

---
+++ baithicuoiky/baithicuoiky/Capnhattruonghoc.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Reflection.Emit;
+
+namespace Deso2
+{
+    public partial class Capnhattruonghhoc : System.Web.UI.Page
+    {
+        public static string chuoiKN = "Data Source=DESKTOP-M3JOAG0\\SQLEXPRESS;Initial Catalog=QL_SINHVIEN;Integrated Security=True;Trust Server Certificate=True";
+        public static SqlConnection cn = new SqlConnection(chuoiKN);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+
+        void ThucThi(string caulenh)
+        {
+            SqlCommand cm = new SqlCommand(caulenh, cn);
+            cn.Open();
+            cm.ExecuteNonQuery();
+            cn.Close();
+        }
+        void HienThi()
+        {
+            try
+            {
+                string chuoiSQL = "SELECT * FROM tbl_truong";
+                SqlDataAdapter da = new SqlDataAdapter(chuoiSQL, cn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            catch (Exception)
+            {
+                Label1.Text = "Lỗi kết nối!";
+            }
+        }
+
+        protected void btnThem_Click(object sender, EventArgs e)
+        {
+            txtMaTH.Text = "";
+            txtTenTH.Text = "";
+        }
+
+        protected void btnLuu_Click(object sender, EventArgs e)
+        {
+            string chuoiSQL = "INSERT INTO tbl_truong values ('" + txtMaTH.Text + "',N'" + txtTenTH.Text + "')";
+            ThucThi(chuoiSQL);
+            HienThi();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string chuoiSQL = "SELECT * FROM tbl_truong";
+            SqlDataAdapter da = new SqlDataAdapter(chuoiSQL, cn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            int dong = GridView1.SelectedIndex;
+            int trang = GridView1.PageIndex;
+            txtMaTH.Text = dt.Rows[trang * 3 + dong][0].ToString();
+            txtTenTH.Text = dt.Rows[trang * 3 + dong][1].ToString();
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+        }
+
+        protected void btnSua_Click(object sender, EventArgs e)
+        {
+            string chuoiSQL = "UPDATE tbl_truong SET TenTruong=N'" + txtTenTH.Text + "'where MaTruong = '" + txtMaTH.Text + "'";
+            ThucThi(chuoiSQL);
+            HienThi();
+        }
+
+        protected void btnXoa_Click(object sender, EventArgs e)
+        {
+            string chuoiSQL = "DELETE tbl_truong where MaTruong='" + txtMaTH.Text + "'";
+            ThucThi(chuoiSQL);
+            HienThi();
+            txtMaTH.Text = "";
+            txtTenTH.Text = "";
+        }
+    }
+}
---
+++ baithicuoiky/baithicuoiky/Dangkythongtin.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace baithicuoiky
+{
+    public partial class Dangkythongtin : System.Web.UI.Page    
+    {
+        public static string chuoiKN = "Data Source=NITRO5\\SQLEXPRESS;Initial Catalog=QL_SANPHAM;Integrated Security=True";
+        public static SqlConnection cn = new SqlConnection(chuoiKN);
+        public Dangkythongtin() { }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+
+        void HienThi()
+        {
+            try
+            {
+                string chuoiSQL = "SELECT * FROM tbl_SanPham";
+                SqlDataAdapter da = new SqlDataAdapter(chuoiSQL, cn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            catch (Exception)
+            {
+                Label1.Text = "Lỗi kết nối!";
+            }
+        }
+
+        protected void btnTimTheoMa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtMaSP.Text == "")
+                {
+                    HienThi();
+                }
+                else
+                {
+                    string chuoiSQL = "SELECT * FROM tbl_SanPham where MaSP = N'" + txtMaSP.Text + "'";
+                    SqlDataAdapter da = new SqlDataAdapter(chuoiSQL, cn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                    txtMaSP.Text = "";
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        protected void btnTimTheoTen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtTenSP.Text == "")
+                {
+                    HienThi();
+                }
+                else
+                {
+                    string chuoiSQL = "SELECT * FROM tbl_SanPham where TenSP = N'" + txtTenSP.Text + "'";
+                    SqlDataAdapter da = new SqlDataAdapter(chuoiSQL, cn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                    txtTenSP.Text = "";
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
